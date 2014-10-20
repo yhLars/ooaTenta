3c669f224612fae4183cb028970f046dd53ae54a
yhLars <lars.ahrlin@yh.nackademin.se>
2014-10-20 10:05:44 +0200

    Upptäckte att vi skulle skapa en repo på GitHub (jag började med en lokal). Kopierar över det jag har och fortsätter ....

---
+++ OOA_Tenta/CrimeHeadlineSystemTests/CrimeHeadlineSystemUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrimeHeadlineSystemTests
+{
+    [TestClass]
+    public class CrimeHeadlineSystemUnitTests
+    {
+        [TestMethod]
+        public void Test_ReadTopCrime_From_Polisen()
+        {
+        }
+
+        [TestMethod]
+        public void Test_ReadTopCrime_From_Utryckning()
+        {
+        }
+
+    }
+}
